a49e3c42589acf146c60bb5244017282fd669b81
Deku <zachdsamson@gmail.com>
2021-08-22 18:09:28 -0700

    tested engineer constructor/methods

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,24 @@
+const { expect, test } = require('@jest/globals');
+const { stringContaining } = require('expect');
+const Engineer = require('../lib/Engineer');
+
+test('create engineer object', () => {
+    const engineer = new Engineer('bill nye', '626', 'billbillbill@yahoo.com', 'billGit');
+
+    expect(engineer.name).toBe('bill nye');
+    expect(engineer.id).toBe('626');
+    expect(engineer.email).toBe('billbillbill@yahoo.com');
+    expect(engineer.github).toBe('billGit');
+});
+
+test('gets engineer github username', () => {
+    const engineer = new Engineer('bill nye', '626', 'billbillbill@yahoo.com', 'billGit');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github));
+});
+
+test('gets the role of engineer', () => {
+    const engineer = new Engineer('bill nye', '626', 'billbillbill@yahoo.com', 'billGit');
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+});
